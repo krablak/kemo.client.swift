1ade1d3c06c0b9ef1afe34c2f6de6b864e142815
krablak <michal.racek@twfrbr.com>
2016-07-14 20:10:49 +0200

    Theme code reorganisation and clean up.

--- KemoApp/KemoApp/UITheme.swift
+++ KemoApp/KemoApp/UITheme.swift
@@ -14,116 +14,26 @@ import Cocoa
  */
 protocol UITheme {
 
+	// Applies color theme on given controller components
 	func apply(mainController: ViewController)
-
+	// Provides text attributes for received message text
 	func receiveTextAttrs() -> [String: AnyObject]
 
+	// Provides text attributes for sent message text
 	func sentTextAttrs() -> [String: AnyObject]
-	
+
+	// Set of basic color types
 	var uiColorTypes: UIColorTypes { get }
 
+	// Set of UI components color types
+	var uiColorComponents: UIColorComponents { get }
+
+	// Set of messages content colors
+	var uiColorContent: UIColorContent { get }
 }
 
 /*
- Default white colored theme.
- */
-public class UIThemeWhite: UITheme {
-
-	// Components colors
-	var bgColor = NSColor.whiteColor()
-	var fieldLightBorderColor = NSColor.init(SRGBRed: 138.0 / 255.0, green: 138.0 / 255.0, blue: 138.0 / 255.0, alpha: 0.3)
-	var textViewBorderColor = NSColor.init(SRGBRed: 238.0 / 255.0, green: 238.0 / 255.0, blue: 238.0 / 255.0, alpha: 1.0)
-	var fieldMessageBorderColor = NSColor.init(SRGBRed: 85.0 / 255.0, green: 86.0 / 255.0, blue: 149.0 / 255.0, alpha: 1.0)
-	var fontColor = NSColor.init(SRGBRed: 51.0 / 255.0, green: 51.0 / 255.0, blue: 51.0 / 255.0, alpha: 1.0)
-
-	// Chat text colors
-	var receivedTextColor = NSColor.init(SRGBRed: 51.0 / 255.0, green: 51.0 / 255.0, blue: 51.0 / 255.0, alpha: 1.0)
-	var sentTextColor = NSColor.init(SRGBRed: 130.0 / 255.0, green: 130.0 / 255.0, blue: 130.0 / 255.0, alpha: 1.0)
-
-	// Basic color types
-	let uiColorTypes = UIColorTypes(
-		success: NSColor.init(SRGBRed: 62.0 / 255.0, green: 169.0 / 255.0, blue: 23.0 / 255.0, alpha: 1.0),
-		error: NSColor.redColor(),
-		neutral: NSColor.init(SRGBRed: 138.0 / 255.0, green: 138.0 / 255.0, blue: 138.0 / 255.0, alpha: 0.3),
-		active: NSColor.init(SRGBRed: 85.0 / 255.0, green: 86.0 / 255.0, blue: 149.0 / 255.0, alpha: 1.0),
-		defaultFont: NSColor.init(SRGBRed: 51.0 / 255.0, green: 51.0 / 255.0, blue: 51.0 / 255.0, alpha: 1.0)
-	)
-
-	func apply(mainController: ViewController) {
-		// Window style
-
-		mainController.mainView!.window?.backgroundColor = bgColor
-		mainController.mainView!.window?.alphaValue = 1
-
-		// Nickname field style
-		mainController.nickFld!.wantsLayer = true
-		mainController.nickFld!.layer?.borderColor = fieldLightBorderColor.CGColor
-		mainController.nickFld!.layer?.backgroundColor = bgColor.CGColor
-		mainController.nickFld!.layer?.cornerRadius = 3
-		mainController.nickFld!.layer?.borderWidth = 1
-		mainController.nickFld!.frame.size.height = 33
-
-		// Key field style
-		mainController.kemoKeyFld!.wantsLayer = true
-		mainController.kemoKeyFld!.layer?.borderColor = fieldLightBorderColor.CGColor
-		mainController.kemoKeyFld!.layer?.backgroundColor = bgColor.CGColor
-		mainController.kemoKeyFld!.layer?.cornerRadius = 3
-		mainController.kemoKeyFld!.layer?.borderWidth = 1
-		mainController.kemoKeyFld!.frame.size.height = 33
-		mainController.kemoKeyFld!.textColor = fontColor
-
-		// Message text view style
-		mainController.messageTextView!.wantsLayer = true
-		mainController.messageTextView!.layer?.borderColor = NSColor.clearColor().CGColor
-		mainController.messageTextView!.layer?.borderWidth = 1
-		mainController.messageTextView!.layer?.cornerRadius = 3
-		// Style scroll view around message text view component
-		mainController.messageTextScrollView!.borderType = NSBorderType.NoBorder
-		mainController.messageTextScrollView!.wantsLayer = true
-		mainController.messageTextScrollView!.layer?.borderColor = textViewBorderColor.CGColor
-		mainController.messageTextScrollView!.layer?.backgroundColor = bgColor.CGColor
-		mainController.messageTextScrollView!.layer?.borderWidth = 1
-		mainController.messageTextScrollView!.layer?.cornerRadius = 3
-		mainController.messageTextScrollView!.layer?.displayIfNeeded()
-
-		// New message field style
-		mainController.messageTextFld!.wantsLayer = true
-		mainController.messageTextFld!.layer?.borderColor = fieldMessageBorderColor.CGColor
-		mainController.messageTextFld!.layer?.backgroundColor = bgColor.CGColor
-		mainController.messageTextFld!.layer?.cornerRadius = 3
-		mainController.messageTextFld!.layer?.borderWidth = 1
-		mainController.messageTextFld!.frame.size.height = 33
-		mainController.messageTextFld!.textColor = uiColorTypes.defaultFont
-
-		// Info view button
-		mainController.infoBtn!.wantsLayer = true
-		mainController.infoBtn!.layer?.backgroundColor = bgColor.CGColor
-		mainController.infoBtn!.layer?.displayIfNeeded()
-		mainController.infoBtn!.image = NSImage.init(named: "NSStatusAvailable")
-		
-		//mainController.infoBtn!.color = uiColorTypes.defaultFont
-
-	}
-
-	func receiveTextAttrs() -> [String: AnyObject] {
-		return [
-			NSFontAttributeName: NSFont.systemFontOfSize(13.0, weight: NSFontWeightLight),
-			NSForegroundColorAttributeName: receivedTextColor,
-			NSBaselineOffsetAttributeName: 5
-		]
-	}
-
-	func sentTextAttrs() -> [String: AnyObject] {
-		return [
-			NSFontAttributeName: NSFont.systemFontOfSize(13.0, weight: NSFontWeightLight),
-			NSForegroundColorAttributeName: sentTextColor,
-			NSBaselineOffsetAttributeName: 5
-		]
-	}
-}
-
-/*
- UITheme specific color types.
+ UITheme basic color types.
  */
 public struct UIColorTypes {
 	// Represents succesful operation or state
@@ -136,5 +46,102 @@ public struct UIColorTypes {
 	let active: NSColor
 	// Default font color
 	let defaultFont: NSColor
+}
+
+/*
+ UITheme components colors.
+ */
+public struct UIColorComponents {
+	// Main background color
+	let bgColor: NSColor
+	// Border color of common fields
+	let borderColor: NSColor
+	// Border color of read-only fields
+	let readOnlyBorderColor: NSColor
+	// Border color of important/highlighted fields
+	let importantBorderColor: NSColor
+}
+
+/*
+ UITheme messages content colors.
+ */
+public struct UIColorContent {
+	// Color of received messages font
+	let receivedTextColor: NSColor
+	// Color of sent messages
+	let sentTextColor: NSColor
+}
+
+/*
+ Extension with default UITheme methods implementations.
+ */
+extension UITheme {
+	func apply(mainController: ViewController) {
+		mainController.mainView!.window?.backgroundColor = uiColorComponents.bgColor
+		mainController.mainView!.window?.alphaValue = 1
+
+		// Nickname field style
+		mainController.nickFld!.wantsLayer = true
+		mainController.nickFld!.layer?.borderColor = uiColorComponents.borderColor.CGColor
+		mainController.nickFld!.layer?.backgroundColor = uiColorComponents.bgColor.CGColor
+		mainController.nickFld!.layer?.cornerRadius = 3
+		mainController.nickFld!.layer?.borderWidth = 1
+		mainController.nickFld!.frame.size.height = 33
+
+		// Key field style
+		mainController.kemoKeyFld!.wantsLayer = true
+		mainController.kemoKeyFld!.layer?.borderColor = uiColorComponents.borderColor.CGColor
+		mainController.kemoKeyFld!.layer?.backgroundColor = uiColorComponents.bgColor.CGColor
+		mainController.kemoKeyFld!.layer?.cornerRadius = 3
+		mainController.kemoKeyFld!.layer?.borderWidth = 1
+		mainController.kemoKeyFld!.frame.size.height = 33
+		mainController.kemoKeyFld!.textColor = uiColorTypes.defaultFont
+
+		// Message text view style
+		mainController.messageTextView!.wantsLayer = true
+		mainController.messageTextView!.layer?.borderColor = NSColor.clearColor().CGColor
+		mainController.messageTextView!.layer?.borderWidth = 1
+		mainController.messageTextView!.layer?.cornerRadius = 3
+		// Style scroll view around message text view component
+		mainController.messageTextScrollView!.borderType = NSBorderType.NoBorder
+		mainController.messageTextScrollView!.wantsLayer = true
+		mainController.messageTextScrollView!.layer?.borderColor = uiColorComponents.readOnlyBorderColor.CGColor
+		mainController.messageTextScrollView!.layer?.backgroundColor = uiColorComponents.bgColor.CGColor
+		mainController.messageTextScrollView!.layer?.borderWidth = 1
+		mainController.messageTextScrollView!.layer?.cornerRadius = 3
+		mainController.messageTextScrollView!.layer?.displayIfNeeded()
+
+		// New message field style
+		mainController.messageTextFld!.wantsLayer = true
+		mainController.messageTextFld!.layer?.borderColor = uiColorComponents.importantBorderColor.CGColor
+		mainController.messageTextFld!.layer?.backgroundColor = uiColorComponents.bgColor.CGColor
+		mainController.messageTextFld!.layer?.cornerRadius = 3
+		mainController.messageTextFld!.layer?.borderWidth = 1
+		mainController.messageTextFld!.frame.size.height = 33
+		mainController.messageTextFld!.textColor = uiColorTypes.defaultFont
+
+		// Info view button
+		mainController.infoBtn!.wantsLayer = true
+		mainController.infoBtn!.layer?.backgroundColor = NSColor.clearColor().CGColor
+		mainController.infoBtn!.layer?.displayIfNeeded()
+		mainController.infoBtn!.image = NSImage.init(named: "NSStatusAvailable")
+
+	}
+	
+	func receiveTextAttrs() -> [String: AnyObject] {
+		return [
+			NSFontAttributeName: NSFont.systemFontOfSize(13.0, weight: NSFontWeightLight),
+			NSForegroundColorAttributeName: uiColorContent.receivedTextColor,
+			NSBaselineOffsetAttributeName: 5
+		]
+	}
+
+	func sentTextAttrs() -> [String: AnyObject] {
+		return [
+			NSFontAttributeName: NSFont.systemFontOfSize(13.0, weight: NSFontWeightLight),
+			NSForegroundColorAttributeName: uiColorContent.sentTextColor,
+			NSBaselineOffsetAttributeName: 5
+		]
+	}
 }
 
